f79f3a8394dfbe5de22248a17a73bb9d9325de7c
yagrabit <156518458+yagrabit@users.noreply.github.com>
2025-11-22 00:04:31 +0900

    feat: PWAサポートを追加し、関連する設定とアイコンを追加しました。

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,35 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Tempwriter',
+        short_name: 'Tempwriter',
+        description: 'A simple, browser-based Markdown editor',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
